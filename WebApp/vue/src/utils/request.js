e5726c40298c927ff616b0efd42061a41904351d
Ji <uie80936@contiwan.com>
2023-10-09 13:30:06 +0800

    common request, same as axios index

--- WebApp/vue/src/utils/request.js
+++ WebApp/vue/src/utils/request.js
@@ -1,14 +1,18 @@
-import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
-import store from '@/store'
-import { getToken } from '@/utils/auth'
+import axios from "axios";
+import { MessageBox, Message } from "element-ui";
+import store from "@/store";
+import { getToken } from "@/utils/auth";
 
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
-})
+  timeout: 10000, // request timeout
+  // headers: {
+  //   "Content-Type": "application/json; charset=utf-8",
+  //   "Accept-Language": "zh-Hans"
+  // }
+});
 
 // request interceptor
 service.interceptors.request.use(
@@ -19,67 +23,169 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers.Authorization = "Bearer " + getToken();
     }
-    return config
+
+    // if (config.method == "POST") {
+    //   config.headers["Content-Type"] =
+    //     "application/x-www-form-urlencoded;charset=UTF-8;";
+    // }
+    return config;
   },
   error => {
     // do something with request error
-    console.log(error) // for debug
-    return Promise.reject(error)
+    console.log(error); // for debug
+    return Promise.reject(error);
   }
-)
+);
 
 // response interceptor
 service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
    * Please return  response => response
-  */
+   */
 
   /**
    * Determine the request status by custom code
    * Here is just an example
    * You can also judge the status by HTTP Status Code
    */
-  response => {
-    const res = response.data
+  res => {
+    /**
+    const res = response.data;
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
+    if (res.status !== 200) {
       Message({
-        message: res.message || 'Error',
-        type: 'error',
+        message: res.message || "Error",
+        type: "error",
         duration: 5 * 1000
-      })
+      });
 
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      if (res.code === 401 ||res.code === 50008 || res.code === 50012 || res.code === 50014) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
+        MessageBox.confirm(
+          "You have been logged out, you can cancel to stay on this page, or log in again",
+          "Confirm logout",
+          {
+            confirmButtonText: "Re-Login",
+            cancelButtonText: "Cancel",
+            type: "warning"
+          }
+        ).then(() => {
+          store.dispatch("user/resetToken").then(() => {
+            location.reload();
+          });
+        });
       }
-      return Promise.reject(new Error(res.message || 'Error'))
+      return Promise.reject(new Error(res.message || "Error"));
     } else {
-      return res
+      return res;
     }
-  },
-  error => {
-    console.log('err' + error) // for debug
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    })
-    return Promise.reject(error)
-  }
-)
 
-export default service
+   */
+    return res;
+  },
+  err => {
+    console.log("err" + error); // for debug
+    // Message({
+    //   message: error.message,
+    //   type: "error",
+    //   duration: 5 * 1000
+    // });
+    // return Promise.reject(error);
+
+    // 404等问题可以在这里处理
+    if (err.response) {
+      const error = (err.error = {});
+      switch (
+        err.response.status // 判断后台返回的token值是否失效
+      ) {
+        case 401:
+          Message({
+            message: "登录过期，请重新登录！",
+            type: "error",
+            duration: 5 * 1000
+          });
+          router.replace({
+            path: "/login"
+          });
+          return;
+
+        case 400:
+          error.code = err.response.data.error.code;
+          error.message = err.response.data.error.message;
+          error.details = err.response.data.error.details;
+          break;
+
+        case 403:
+          error.code = err.response.data.error.code;
+          error.message = err.response.data.error.message;
+          error.details = err.response.data.error.details;
+          break;
+
+        case 404:
+          if (!err.response.data) {
+            error.code = "未找到服务！";
+          } else {
+            error.code = err.response.data.error.code;
+            error.message = err.response.data.error.message;
+            error.details = err.response.data.error.details;
+          }
+          break;
+
+        case 408:
+          error.code = err.response.data.error.code;
+          error.message = err.response.data.error.message;
+          error.details = err.response.data.error.details;
+          break;
+
+        case 500:
+          error.code = err.response.data.error.code;
+          error.message = err.response.data.error.message;
+          error.details = err.response.data.error.details;
+          break;
+
+        case 501:
+          error.code = err.response.data.error.code;
+          error.message = err.response.data.error.message;
+          error.details = err.response.data.error.details;
+          break;
+
+        case 502:
+          error.code = "502 Bad Gateway";
+          break;
+
+        case 503:
+          error.code = err.response.data.error.code;
+          error.message = err.response.data.error.message;
+          error.details = err.response.data.error.details;
+          break;
+
+        case 504:
+          error.code = err.response.data.error.code;
+          error.message = err.response.data.error.message;
+          error.details = err.response.data.error.details;
+          break;
+
+        case 505:
+          error.code = err.response.data.error.code;
+          error.message = err.response.data.error.message;
+          error.details = err.response.data.error.details;
+          break;
+
+        default:
+      }
+      return Promise.reject(err);
+    } else if (err.request) {
+      return Promise.reject(err.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      return Promise.reject("Error", err.message);
+    }
+  }
+);
+
+export default service;
